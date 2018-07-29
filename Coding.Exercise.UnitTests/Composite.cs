cf61c7784df5d5e8a6c25ce883fc0a9a1527505c
MichaelR <MichaelR@tenisoft.com>
2018-07-29 15:10:26 +0300

    Composite

---
+++ Coding.Exercise.UnitTests/Composite.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Composite.Coding.Exercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Coding.Exercise.UnitTests
+{
+    [TestClass]
+    public class Composite
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var singleValue = new SingleValue {Value = 11};
+            var otherValues = new ManyValues();
+            otherValues.Add(22);
+            otherValues.Add(33);
+            Assert.AreEqual(new List<IValueContainer> {singleValue, otherValues}.Sum(), 66);
+        }
+    }
+}
