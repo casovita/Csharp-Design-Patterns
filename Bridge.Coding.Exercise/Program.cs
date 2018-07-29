a7712e22f11a47de3c50d330b6bf7f4f519e646f
MichaelR <MichaelR@tenisoft.com>
2018-07-29 11:02:23 +0300

    Bridge CE

---
+++ Bridge.Coding.Exercise/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Bridge.Coding.Exercise
+{
+    public interface IRenderer
+    {
+        string WhatToRenderAs { get; }
+    }
+
+    public abstract class Shape
+    {
+        private IRenderer rendering;
+
+        protected Shape(IRenderer rendering)
+        {
+            this.rendering = rendering;
+        }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Drawing {Name} as {rendering.WhatToRenderAs}";
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public Triangle(IRenderer strategy) : base(strategy)
+        {
+            Name = "Triangle";
+        }
+    }
+
+    public class Square : Shape
+    {
+        public Square(IRenderer rendering) : base(rendering)
+        {
+            Name = "Square";
+        }
+    }
+
+    public class RasterRenderer : IRenderer
+    {
+        public string WhatToRenderAs
+        {
+            get { return "pixels"; }
+        }
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        public string WhatToRenderAs
+        {
+            get { return "lines"; }
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Coding.Exercise.UnitTests/Bridge.cs
@@ -0,0 +1,16 @@
+﻿using Bridge.Coding.Exercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Coding.Exercise.UnitTests
+{
+    [TestClass]
+    public class Bridge
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var sq = new Square(new VectorRenderer()).ToString();
+            Assert.AreEqual(sq, "Drawing Square as lines","Invalid");
+        }
+    }
+}
