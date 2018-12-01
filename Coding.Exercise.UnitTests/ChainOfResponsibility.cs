cfb82b127c85ea944ec6d9a8ef32c215160633b4
roman <casovita@gmail.com>
2018-12-01 14:36:42 +0200

    Chain of resposibility

---
+++ Coding.Exercise.UnitTests/ChainOfResponsibility.cs
@@ -0,0 +1,32 @@
+﻿using ChainOfResponsibility.Coding.Exercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Coding.Exercise.UnitTests
+{
+    [TestClass]
+    public class ChainOfResponsibility
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var game = new Game();
+            var goblin = new Goblin(game);
+            game.Creatures.Add(goblin);
+
+            Assert.AreEqual(goblin.Attack, 1);
+            Assert.AreEqual(goblin.Defense, 1);
+
+            var goblin2 = new Goblin(game);
+            game.Creatures.Add(goblin2);
+
+            Assert.AreEqual(goblin.Attack, 1);
+            Assert.AreEqual(goblin.Defense, 2);
+
+            var goblin3 = new GoblinKing(game);
+            game.Creatures.Add(goblin3);
+
+            Assert.AreEqual(goblin.Attack, 2);
+            Assert.AreEqual(goblin.Defense, 3);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Csharp-Design-Patterns
+**Motivation to use**
+
+Behavioral: 
+<ol>
+<li>Chain of Responsibility <i>(includes mediator pattern)</i>
+<p>A chain of components who all get a chance to process a command or query, optionally having default processing implementation and an ability to terminate the processing chain.</p>
+<p>Can be implemented as a chain of references of a centralized construct.
+Enlist objects in the chain, possibly controlling their order.
+Object removal from the chain. (e.g., in Dispose())</p>
+</li>
+<li>Command
+<p>An object which represents an instruction to perform a particular action. Contains all the information necessary for the action to be taken.<br>
+Uses: want an object that represents operation, GUI commands, multi-level undo/redo, macro recording and more.</p></li>
+</ol>
